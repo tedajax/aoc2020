bc3da651d6f0f3e30478a762db0327163ce3f700
Ted Dobyns <ted.ajax@gmail.com>
2020-12-15 11:58:38 -0800

    more day 15 cleanup

--- puzzle_15.c
+++ puzzle_15.c
@@ -1,51 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define STB_DS_IMPLEMENTATION
-#include "stb_ds.h"
+int play_game(int* numbers, size_t len, int max_turn)
+{
+    int* map = (int*)calloc(max_turn, sizeof(int));
+
+    int turn = 1;
+    int spoken = 0;
+    while (turn <= max_turn) {
+        int prev = spoken;
+
+        if (turn <= len) {
+            spoken = numbers[turn - 1];
+        } else {
+            if (map[spoken] == 0) {
+                spoken = 0;
+            } else {
+                spoken = (turn - 1) - map[spoken];
+            }
+        }
+
+        map[prev] = turn++ - 1;
+    }
+
+    free(map);
+
+    return spoken;
+}
 
 int main(int argc, char* argv[])
 {
     enum { LEN = 7 };
     int numbers[LEN] = {7, 14, 0, 17, 11, 1, 2};
-    struct {
-        int key;
-        int value;
-    } * map[2] = {NULL};
 
-    int turn = 1;
-    int spoken = 0;
-    int max_turn = 30000000;
-    while (turn <= max_turn) {
-        if (turn <= LEN) {
-            spoken = numbers[turn - 1];
-        } else {
-            if (hmgeti(map[1], spoken) < 0) {
-                spoken = 0;
-            } else {
-                spoken = hmget(map[0], spoken) - hmget(map[1], spoken);
-            }
-        }
-
-        if (hmgeti(map[0], spoken) >= 0) {
-            hmput(map[1], spoken, hmget(map[0], spoken));
-        }
-        hmput(map[0], spoken, turn);
-
-        if (max_turn / 100 > 0) {
-            if (turn % (max_turn / 100) == 0) {
-                printf(
-                    " processing........................... %2.0f%%\r", turn * 100.0f / max_turn);
-            }
-        }
-
-        ++turn;
-    }
-
-    printf("\n%d\n", spoken);
+    printf("2020: %d\n", play_game(numbers, LEN, 2020));
+    printf("30000000: %d\n", play_game(numbers, LEN, 30000000));
     getc(stdin);
-
-    hmfree(map[0]);
-    hmfree(map[1]);
 
     return 0;
 }
