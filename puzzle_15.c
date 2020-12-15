e3368b135ba2f928f25e4b48bbdd8d15e0f31b73
Ted Dobyns <ted.ajax@gmail.com>
2020-12-15 12:13:46 -0800

    final cleanup day 15

--- puzzle_15.c
+++ puzzle_15.c
@@ -1,40 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int play_game(int* numbers, size_t len, int max_turn)
+int map[30000000];
+
+int play_game(int max_turn)
 {
-    int* map = (int*)calloc(max_turn, sizeof(int));
+    enum { LEN = 7 };
+    int numbers[LEN] = {7, 14, 0, 17, 11, 1, 2};
 
-    int turn = 1;
-    int spoken = 0;
-    while (turn <= max_turn) {
-        int prev = spoken;
+    memset(map, 0, sizeof(map));
 
-        if (turn <= len) {
-            spoken = numbers[turn - 1];
-        } else {
-            if (map[spoken] == 0) {
-                spoken = 0;
-            } else {
-                spoken = (turn - 1) - map[spoken];
-            }
-        }
-
-        map[prev] = turn++ - 1;
+    for (size_t i = 0; i < LEN; ++i) {
+        map[numbers[i]] = i + 1;
     }
 
-    free(map);
+    int turn = LEN;
+    int spoken = numbers[LEN - 1];
+    while (turn < max_turn) {
+        int last = map[spoken];
+        map[spoken] = turn;
+        spoken = last ? turn - last : 0;
+        turn++;
+    }
 
     return spoken;
 }
 
 int main(int argc, char* argv[])
 {
-    enum { LEN = 7 };
-    int numbers[LEN] = {7, 14, 0, 17, 11, 1, 2};
-
-    printf("2020: %d\n", play_game(numbers, LEN, 2020));
-    printf("30000000: %d\n", play_game(numbers, LEN, 30000000));
+    printf("2020: %d\n", play_game(2020));
+    printf("30000000: %d\n", play_game(30000000));
     getc(stdin);
 
     return 0;
